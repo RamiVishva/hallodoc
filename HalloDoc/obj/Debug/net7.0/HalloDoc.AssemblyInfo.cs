fbca923a0bfb08ae62fb6c23c7939b7b97b71f69
RamiVishva <vishvarami027@gmail.com>
2024-02-25 22:36:20 +0530

    Admin dashboard

--- HalloDoc/obj/Debug/net7.0/HalloDoc.AssemblyInfo.cs
+++ HalloDoc/obj/Debug/net7.0/HalloDoc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HalloDoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329cb52125fbbd4167a2c62f867eec357ac7e059")]
 [assembly: System.Reflection.AssemblyProductAttribute("HalloDoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HalloDoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
